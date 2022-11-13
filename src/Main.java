99539d0781c62efc4f5939b2ae2354ea4d7673a6
kymdennis3 <kymdennis3@gmail.com>
2022-11-13 19:51:26 +0300

    example - the variable changes because we used double to declare it

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //final - once a variable is declared it cant be changed
+
+        double pi = 3.14;
+
+        pi = 6;
+
+        System.out.println(pi);
     }
 }
